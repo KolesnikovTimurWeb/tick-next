be76f611f392466c59dfea7ff65651006c9dbea8
KolesnikovTimurWeb <kolesnikovtimyrweb@gmail.com>
2024-06-10 19:05:12 +0300

    vercel error

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,9 +1,5 @@
 import styles from "./page.module.css";
-import { getStaticProps } from "./page";
-
 export default function Home() {
-   const posts = getStaticProps();
-   console.log(posts);
    return (
       <main className={styles.main}>
 
