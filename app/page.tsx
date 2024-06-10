a2d5a97241e6ee9ad51d0c58bb20516144ebc3a3
KolesnikovTimurWeb <kolesnikovtimyrweb@gmail.com>
2024-06-10 19:02:06 +0300

    vercel err

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import prisma from "../lib/prisma";
-import { GetStaticProps } from "next";
 
-const getStaticProps: GetStaticProps = async () => {
-  const feed = await prisma.user.findMany();
-  console.log(feed)
-  return {
-    props: { feed },
-    revalidate: 10,
-  };
-};
 export default async function Home() {
+
+  const getStaticProps = async () => {
+    const feed = await prisma.user.findMany();
+    console.log(feed)
+    return {
+      props: { feed },
+      revalidate: 10,
+    };
+  };
+
   const posts = await getStaticProps()
   console.log({ posts })
   return (
