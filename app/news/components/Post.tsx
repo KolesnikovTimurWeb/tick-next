5f35d951bc6fa7cb990ae196ad58bceaf135ba2f
KolesnikovTimurWeb <kolesnikovtimyrweb@gmail.com>
2024-06-20 14:35:13 +0300

    array type

--- app/news/components/Post.tsx
+++ app/news/components/Post.tsx
@@ -2,14 +2,13 @@
 import styles from "@/app/styles/News.module.scss"
 
    interface PostProps{
-      item:object,
+      item: ,
    }
    const Post = ({item}:PostProps) => {
      return (
        <div className={styles.news_post}>  
          <div className={styles.news_post_text}>
          <h2>{item.title}</h2>
-         <h4>{item.desc}</h4>
 
          </div>
        </div>
