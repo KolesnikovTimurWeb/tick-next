8d6c119c3e5d17918f1fa8b70c53d969303b3fad
KolesnikovTimurWeb <kolesnikovtimyrweb@gmail.com>
2024-07-12 00:55:44 +0300

    Solve useGetPosts type issue

--- app/hooks/useGetPosts.tsx
+++ app/hooks/useGetPosts.tsx
@@ -1,36 +1,31 @@
 "use client"
 
 import axios from "axios"
+import { Group } from "next/dist/shared/lib/router/utils/route-regex"
 import { useInfiniteQuery, useQuery } from "react-query"
 
-const useGetPosts =() => {
+interface News {
 
-   const {data,isLoading,fetchNextPage} = useInfiniteQuery({
-      queryKey:['news'],
-      queryFn: async({pageParam = 1})=>{
-         const {data} = await  axios.get(`https://newsapi.org/v2/everything?q=REACT.js&page=${pageParam}&language=en&sortBy=relevancy&pageSize=10&apiKey=501d70ab741c433b99e394a6d8129295`)
-         if(data.articles < 1) return null
-         return data.articles
-      },
-      initialPageParam:1,
-      getNextPageParam:(_,pages) => {
-         return pages.length + 1
-      },
-      staleTime: 1000 * 30, // 30s 
-      refetchOnWindowFocus: false,
-    refetchOnMount: false,
-   })
-   // const {data,isLoading} = useQuery({
-   //    queryKey:['news'],
-   //    queryFn: async()=>{
-   //       const {data} = await  axios.get('https://newsapi.org/v2/everything?q=REACT.js&page=2&language=en&sortBy=relevancy&pageSize=10&apiKey=501d70ab741c433b99e394a6d8129295')
-   //       return data.articles
-   //    },
-   //    staleTime: 1000 * 30, // 30s 
-   //    refetchOnWindowFocus: false,
-   //  refetchOnMount: false,
-   // })
-   console.log(data)
+   id: number;
+   name: string;
+ }
+
+const useGetPosts =() => {
+   const fetchNews = async ({ pageParam = 1 }) => {
+      const { data } = await axios.get(`https://newsapi.org/v2/everything?q=REACT.js&page=${pageParam}&language=en&sortBy=relevancy&pageSize=10&apiKey=501d70ab741c433b99e394a6d8129295`);
+      if (data.articles.length < 1) return null;
+      return data.articles;
+    };
+   const { data, isLoading, fetchNextPage } = useInfiniteQuery(
+      'news',
+      fetchNews,
+      {
+        getNextPageParam: (_, pages) => pages.length + 1,
+        staleTime: 1000 * 30, // 30 seconds
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+      }
+    )
    return {data,isLoading,fetchNextPage}
 }
 
